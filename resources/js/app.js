844e4865cf761847270a3dfaf54a4855ad200bf6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-06 10:58:52 +0900

    import & using router/index.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,9 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import Router from './router/index';
+
 const app = new Vue({
     el: '#app',
+    router: Router
 });
